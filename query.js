fb894b05f79121d4427203995e49bc4ee18e4234
sani <khursani8@gmail.com>
2017-07-25 20:11:52 +0800

    group by current_stop

--- query.js
+++ query.js
@@ -32,7 +32,7 @@ where c.client_id = 5 /*  */
 WHERE e.bus_no = ? and e.route_id = 2 and not e.current_stop in (0);
 `
 
-var information = `select b.bus_no, locstart.location_name as "Current Stop", r.bus_time,locend.location_name as "Next Stop"
+var information = `select b.bus_no, locstart.location_name as "current_Stop", r.bus_time,locend.location_name as "next_Stop"
 FROM route r
 LEFT JOIN bus b
 on b.bus_route = r.route_id
@@ -41,7 +41,7 @@ on locstart.location_position = r.current_stop
 LEFT JOIN location locend
 on locend.location_position = r.next_stop
 where b.bus_no = ? and r.route_id = 2
-group by current_stop
+group by current_stop;
 `
 
 module.exports = {
